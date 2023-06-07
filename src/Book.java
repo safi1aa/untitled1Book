de5ba6bd13096ba9295bcf572cd23bf1c978e9ea
User <egemberdievasafia@gmail.com>
2023-06-07 18:18:42 +0600

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    String name;
+    String author;
+    int year;
+    public void book(){
+        System.out.println(name);
+        System.out.println(author);
+        System.out.println(year);
+        
+    }
+    }
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Book book = new Book();
+        book.name = "The villain's only outcome is death.";
+        book.author = "Gwon Gyeoeul";
+        book.year = 2020;
+        book.book();
+    }
+}
