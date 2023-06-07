634e92490fe1829d015390a4ceb62c174bc84b00
User <egemberdievasafia@gmail.com>
2023-06-07 19:30:07 +0600

    commit

---
+++ src/Book.java
@@ -0,0 +1,10 @@
+public class Book {
+    String name;
+    String author;
+    int year;
+    public static void Book(String name, String author, int year){
+        System.out.println("Name: "+name);
+        System.out.println("author: "+author);
+        System.out.println("year: "+year);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,10 @@ import com.sun.tools.attach.VirtualMachine;
 
 public class Main {
     public static void main(String[] args) {
-        Counter counter = new Counter();
-        counter.getValue();
-        System.out.println(counter.getValue());
-        counter.c();
-        System.out.println(counter.getValue());
-        counter.s();
-        System.out.println(counter.getValue());
+        Book book = new Book();
+        book.name = "Villains Are Destined to Die";
+        book.author = "Gwon Gyeoeul";
+        book.year = 2020;
+        Book.Book(book.name, book.author, book.year);
     }
 }
